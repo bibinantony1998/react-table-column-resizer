f17aba3ea600b5610350126ef32b6e05a0026900
Bibin antony <>
2023-06-01 17:31:15 +0530

    disabled bug fix

--- demo/src/App.js
+++ demo/src/App.js
@@ -9,11 +9,11 @@ export const App = () => {
         <thead>
           <tr>
             <th>1</th>
-            <ColumnResize id={1} maxWidth={300} resizeEnd={(width)=> console.log("resize end", width)} resizeStart={()=> console.log("resize start")} className="columnResizer" />
+            <ColumnResize id={1} resizeEnd={(width)=> console.log("resize end", width)} resizeStart={()=> console.log("resize start")} className="columnResizer" />
             <th>2</th>
-            <ColumnResize id={2} maxWidth={300} resizeEnd={(width)=> console.log("resize end", width)} resizeStart={()=> console.log("resize start")} className="columnResizer" />
-            <th>3</th>
-            <ColumnResize id={3} resizeEnd={(width)=> console.log("resize end", width)} resizeStart={()=> console.log("resize start")} className="columnResizer" />
+            <ColumnResize id={2} resizeEnd={(width)=> console.log("resize end", width)} resizeStart={()=> console.log("resize start")} className="columnResizer" />
+            <th>kjbslidhvsidvsjdbvjkbskjdvskjdvbskdvjksdv</th>
+            <ColumnResize disabled id={3} minWidth={400} resizeEnd={(width)=> console.log("resize end", width)} resizeStart={()=> console.log("resize start")} className="columnResizer" />
             <th>minWidth 120 </th>
             <ColumnResize id={4} minWidth={120} className="columnResizer" />
             <th>5</th>
