0db5682868df46bf61a5f312b9d338e678145990
bibin antony <bibinantony1998@gmail.com>
2024-08-31 00:35:42 +0530

    colSpan and row span in props

--- demo/src/App.js
+++ demo/src/App.js
@@ -8,30 +8,27 @@ export const App = () => {
       <table className="column_resize_table">
         <thead>
           <tr>
-            <th>1</th>
-            <ColumnResize id={1} defaultWidth={400} minWidth={50}  resizeEnd={(width)=> console.log("resize end", width)} resizeStart={()=> console.log("resize start")} className="columnResizer" />
-            <th>2</th>
-            <ColumnResize id={2} resizeEnd={(width)=> console.log("resize end", width)} resizeStart={()=> console.log("resize start")} className="columnResizer" />
-            <th>xsxsxsx</th>
-            <ColumnResize disabled id={3} minWidth={200} maxWidth={200} resizeEnd={(width)=> console.log("resize end", width)} resizeStart={()=> console.log("resize start")} className="columnResizer" />
-            <th>minWidth 120 </th>
-            <ColumnResize id={4} maxWidth={200}  minWidth={120} className="columnResizer" />
-            <th>5</th>
-            <ColumnResize id={4} maxWidth={200} minWidth={120} className="columnResizer" />
+           <th rowSpan={2}>main</th>
+           <ColumnResize rowSpan={2} id={1} defaultWidth={400} minWidth={50}  resizeEnd={(width)=> console.log("resize end", width)} resizeStart={()=> console.log("resize start")} className="columnResizer" />
+           <th rowSpan={2}>11</th>
+           <ColumnResize rowSpan={2} id={1} defaultWidth={400} minWidth={50}  resizeEnd={(width)=> console.log("resize end", width)} resizeStart={()=> console.log("resize start")} className="columnResizer" />
+           <th colSpan={3}>head</th>
           </tr>
-          </thead>
+          <tr>
+            <th>21</th>
+            <ColumnResize id={1} defaultWidth={400} minWidth={50}  resizeEnd={(width)=> console.log("resize end", width)} resizeStart={()=> console.log("resize start")} className="columnResizer" />
+            <th>22</th>
+          </tr>
+        </thead>
         <tbody>  
           <tr>
-            <td>1</td>
+            <td>main</td>
+            <td className="column_resizer_body" />
+            <td>normal</td>
             <td className="column_resizer_body" />
             <td>2</td>
             <td className="column_resizer_body" />
             <td>3</td>
-            <td className="column_resizer_body" />
-            <td>4</td>
-            <td className="column_resizer_body" />
-            <td>5</td>
-            <td className="column_resizer_body" />
           </tr>
         </tbody>
       </table>
