a12740e5b534ae9aeda236db9128db992f7867d7
Noah Overcash <novercash@ebsco.com>
2024-09-16 16:09:19 -0400

    Declare optional props as optional in TS
    The current typings cause errors when trying to use the library without providing all props. This PR marks all props, except `id`, as optional.

--- dist/column-resizer.d.ts
+++ dist/column-resizer.d.ts
@@ -1,21 +1,18 @@
 import { JSX } from "react";
 
 declare module "react-table-column-resizer" {
+  export interface Props {
+    minWidth?: number;
+    maxWidth?: number | null;
+    id: number;
+    resizeStart?: () => void;
+    resizeEnd?: (width: number) => void;
+    className?: string;
+    disabled?: boolean;
+    defaultWidth?: number;
+    rowSpan?: number;
+    colSpan?: number;
+  }
 
-
-	export interface Props {
-        minWidth: number;
-        maxWidth: number | null;
-        id: number;
-        resizeStart(): void;
-        resizeEnd(width: number): void;
-        className: string;
-        disabled: boolean;
-        defaultWidth?: number;
-        rowSpan?: number;
-        colSpan?: number;
-      }
-    
-    export default function ColumnResizer(props: Props): JSX.Element;
-
+  export default function ColumnResizer(props: Props): JSX.Element;
 }
